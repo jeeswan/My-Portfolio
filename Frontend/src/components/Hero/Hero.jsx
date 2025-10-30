8779d1114e431612f2920c0fb4c4d363b767e426
Jeeswan Bajracharya <119114279+jeeswan@users.noreply.github.com>
2025-10-30 12:51:57 +0545

    Update Hero.jsx

--- Frontend/src/components/Hero/Hero.jsx
+++ Frontend/src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I'm Jeeswan</h1>
-            <p className={styles.description}>A passionate developer seeking an internship opportunity</p>
+            <p className={styles.description}>A developer who enjoys designing intuitive interfaces and bringing ideas to life through code.</p>
             <a href="mailto:bajrajeeswan@gmail.com" className={styles.contactbtn}>Contact Me</a>
         </div>
         <img src={assets.hero} alt="heroimage" className={styles.heroimg}/>
